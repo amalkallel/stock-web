06a573ddd4a2351d98121474db8f9334a0ef55a9
Amal Kallel <amal.kallel@gmail.com>
2017-03-09 21:33:20 +0100

    third commit

---
+++ stock-web-app/src/com/stock/data/Fournisseur.java
@@ -0,0 +1,10 @@
+package com.stock.data;
+
+public class Fournisseur {
+	private int id_fournisseur;
+	private String nom;
+	private String prenom;
+	private String tel;
+	
+
+}
---
+++ stock-web-app/src/com/stock/data/Order.java
@@ -0,0 +1,15 @@
+package com.stock.data;
+
+public class Order {
+	
+	private String code;
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	} 
+
+}
