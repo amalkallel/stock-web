2e0ce45922e18ca394cdaa5ac7c86bfc70b01617
Amal Kallel <amal.kallel@gmail.com>
2017-03-09 21:22:33 +0100

    Second commit

---
+++ stock-web-app/src/com/stock/data/Product.java
@@ -0,0 +1,16 @@
+package com.stock.data;
+
+public class Product {
+	
+	private String code;
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+	
+
+}
